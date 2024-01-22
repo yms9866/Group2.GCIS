851747b500fc8689b3999a5d1e07bbcd8ea512c7
Yosef Mogesie <yms9866@rit.edu>
2024-01-22 21:45:01 +0400

    Version1

---
+++ Happybirthday.py
@@ -0,0 +1,12 @@
+def my_birthday(k):
+    i = input('What is your name? ')
+    age = int(input('Your age '))
+    j = 2024 - age
+    c = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
+    l = c[3]
+    print(i+', '+ 'your birth day is in',j,str(k)+'th of',l+'.')
+
+my_birthday(21)
+
+
+   
