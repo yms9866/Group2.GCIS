ea1787149aa28c3dc2aef4b4f95d7354e9f76164
Yosef Mogesie <yms9866@rit.edu>
2024-01-22 22:11:01 +0400

    Version2

--- Happybirthday.py
+++ Happybirthday.py
@@ -1,12 +1,14 @@
-def my_birthday(k):
+def my_birthday():
+    print('Hey I am going to tell you your birth day. please inter the approprate data for the following questions.')
     i = input('What is your name? ')
     age = int(input('Your age '))
     j = 2024 - age
     c = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
     l = c[3]
+    k = input('What is the date today? ')
     print(i+', '+ 'your birth day is in',j,str(k)+'th of',l+'.')
 
-my_birthday(21)
+my_birthday()
 
 
    
