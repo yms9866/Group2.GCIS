93f769e24e16d952344baf61ccea9262e0f5fedb
Yosef Mogesie <yms9866@rit.edu>
2024-01-22 23:35:04 +0400

    Version3

--- Happybirthday.py
+++ Happybirthday.py
@@ -1,14 +1,41 @@
 def my_birthday():
-    print('Hey I am going to tell you your birth day. please inter the approprate data for the following questions.')
+    print('Hey I am going to tell you your birth day. please enter the approprate data for the following questions.')
     i = input('What is your name? ')
-    age = int(input('Your age '))
-    j = 2024 - age
-    c = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
-    l = c[3]
-    k = input('What is the date today? ')
-    print(i+', '+ 'your birth day is in',j,str(k)+'th of',l+'.')
+    age = int(input('Your age in days '))
+    j = age%365
+    a = 365 - j
+    f = a//30
+    k = int(input('What is the date today? '))
+    x = input('And month ')
+    if x == 'January':
+        z = 0 + f
+    elif x == 'February':
+        z = 1 + f
+    elif x == 'March':
+        z = 2 + f
+    elif x == 'April':
+        z = 3 + f
+    elif x == 'May':
+        z = 4 + f
+    elif x == 'June':
+        z = 5 + f
+    elif x == 'July':
+        z = 6 + f
+    elif x == 'August':
+        z = 7 + f
+    elif x == 'September':
+        z = 8 + f
+    elif x == 'October':
+        z = 9 + f
+    elif x == 'November':
+        z = 10 + f
+    elif x == 'December':
+        z = 11 + f
+    g = a-f*30
+    h = k + g
+    if h > 30:
+        s = h - 30
+    
+    print(i+', '+ 'your birth day is in','2024',str(s)+'th of',x+'.')
 
 my_birthday()
-
-
-   
